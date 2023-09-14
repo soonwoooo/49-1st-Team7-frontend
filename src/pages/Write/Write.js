0cc2c9aae470bb0f2af08af92c7fa56917d2512d
soonwoooo <kkshjs123@gmail.com>
2023-09-14 22:22:40 +0900

    Write updated

---
+++ src/pages/Write/Write.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Write.scss';
+
+const Write = () => {
+  const [saveText, setSaveText] = useState('');
+  const [userdata, setUserData] = useState([]);
+  const navigate = useNavigate();
+  const goToBack = () => {
+    navigate('/main');
+  };
+
+  const handleSaveText = e => {
+    setSaveText(e.target.value);
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:8000/posts')
+      .then(res => res.json())
+      .then(data => {
+        setUserData(data);
+      });
+  }, []);
+
+  const handleCommit = () => {
+    if (saveText.length < 1) {
+      alert('내용을 입력해 주세요.');
+
+      return;
+    }
+
+    fetch('http://localhost:8000/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: localStorage.getItem('token'),
+      },
+      body: JSON.stringify({
+        content: { saveText },
+      }),
+    })
+      .then(res => res.json())
+      .then(result => {
+        if (result.message === 'post_success') {
+          alert('게시글이 작성되었습니다');
+
+          goToBack();
+
+          //   if()
+          // //  confirm("ok")
+          // goToBack();
+        }
+      });
+  };
+
+  return (
+    <div className="writeContainer">
+      <div className="contentAndButtonWrapper">
+        <div className="publishWrapper">
+          <div className="contentWrapper">
+            <p>nickname</p>
+            <textarea
+              placeholder="스레드를 시작하세요"
+              onChange={handleSaveText}
+            />
+          </div>
+        </div>
+        <div className="buttonWrapper">
+          <button className="cancle" onClick={goToBack}>
+            취소
+          </button>
+          <button className="commit" onClick={handleCommit}>
+            게시
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Write;
